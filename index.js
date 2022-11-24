e625b2549246cb4053405936b248d4a82d3e1eab
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2022-11-24 22:15:40 +0300

    Correction

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1=2;
+let num2=31;
+let multiply=num1*num2;
 
-// Write your code here
+console.log(multiply);
+
+
+
+let num3 = 14;
+let num4=5;
+let mod;
+
+mod=num3 % num4;
+console.log(mod);
+
+let max=Math.max(11, 0, 18, 3, 20, 1, -3);
+console.log(max);
+
+let random=Math.floor(Math.random() * 10) + 1;
+console.log(random);
